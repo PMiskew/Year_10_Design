00b21e11f6666a55a0c621a3fbd3434e2154db1f
PMiskew <paul.miskew@gmail.com>
2020-09-11 09:45:01 -0400

    update

--- Tools/toolsPM.py
+++ Tools/toolsPM.py
@@ -13,23 +13,82 @@ def isEven(a):
 Codingbat Problem
 '''
 def missing_char(str, n):
+	
+	return str[0:n] + str[n + 1:len(str)]
   
-	front = str[0:n]
-	back = str[n + 1:]
-	result = front + back
-	return result
+  
+'''
+This function takes a single positive integer parameter and returns
+the sum of the digits
+Parameters: i >= 0
+return: returns sum of the digits
+
+preondition: i is a valid integer greater than 0
+'''
+def sumDigits(a):
+
+	total = 0
+	#Casting is the process of changing type. 
+	a = str(a)  
+
+	#Fundamental skill: Looping through string
+
+	#Count, check, change
+	for i in range(0, len(a),1):
+		total = total + int(a[i])
+
+	return total
+	#Trace Table:
+	# a = "1234"
+	# i | i < len(a) | 
+	# 0	| 0 < 4 	 |	T RUN LOOP  total = 0 + a[0] = 0 + "1"
+	# 1 | 1 < 4		 |  T RUN LOOP  total = 1 + a[1] = 1 + "2"
+	# 2 | 2 < 4		 |  T RUN LOOP	total = 3 + a[2] = 3 + "3"
+	# 3 | 3 < 4		 |  T RUN LOOP  total = 6 + a[3] = 6 + "4" = 10
+	# 4 | 4 < 4		 |  F EXIT LOOP
+
+'''
+This function takes a single positive integer parameter and returns
+the sum of the digits
+Parameters: i >= 0
+return: returns sum of the digits
+
+preondition: i is a valid integer greater than 0
+'''
+def sumDigitsA(a):
+
+	total = 0
+
+	while (a > 0):
+		total = total + a % 10 #access the ones digit
+		a = a // 10
+
+	#Trace
+	# a = 57
+	#
+	#  a |  a > 0 |
+	# 57 |  57> 0 | TRUE RUN LOOP total = 0 + 7, a = 5
+	# 5  |  5 > 0 | TRUE RUN LOOP total = 7 + 5, a = 0
+	# 0  |  0 > 0 | FALSE EXIT LOOP
+
+	return total
+
+print(sumDigitsA(57))
 
 
 
+
+'''
 #Test Code for missing_char
 
 print(missing_char('kitten', 1))
 print(missing_char('kitten', 0))
 print(missing_char('kitten', 4))
-
-
-#Test Code for isEven
 '''
+
+'''
+#Test Code for isEven
+
 print(isEven(0))
 print(isEven(10))
 print(isEven(9))
