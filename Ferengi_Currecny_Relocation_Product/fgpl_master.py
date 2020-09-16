8e459114df8c5fd38d89fdd2442c2bd6bb0f792a
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2020-09-16 09:24:23 -0400

    update fgpl

--- Ferengi_Currecny_Relocation_Product/fgpl_master.py
+++ Ferengi_Currecny_Relocation_Product/fgpl_master.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-root = tk.root()
+root = tk.Tk()
 
 
 #frames
@@ -10,10 +10,24 @@ f3 = tk.Frame(root)
 f4 = tk.Frame(root)
 
 
+#Frame 1 Setup
+# Note everything is being placed in Frame 1 and we are simply packing
+
+cur_label = tk.Label(f1, text = "Select Currency")
+variable = tk.StringVar(root)
+variable.set("one") # default value
+
+cur_select = tk.OptionMenu(f1, variable, "one", "two", "three")
+cur_select.pack()
+
+
+
+
+#Frame Placements
 f1.grid(row = 0, column = 0)
 f2.grid(row = 1, column = 0)
 f3.grid(row = 1, column = 1, rowspan = 2)
-f4.grid(row = 2, column = 0, columspan = 2)
+f4.grid(row = 2, column = 0, columnspan = 2)
 
 
 
