bb16c6ace70db452fc67cebfd42190d9528e9b7d
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2020-09-24 11:19:30 -0400

    update

--- Diversity_IO_Product/diversity_data_API.py
+++ Diversity_IO_Product/diversity_data_API.py
@@ -5,7 +5,9 @@ from pprint import pprint
 fname = input("What is the first name: ")
 lname = input("What is the last name:")
 
-resp = requests.get("https://api.diversitydata.io/?fullname="+fname+"%20"+lname)
+#resp = requests.get("https://api.diversitydata.io/?fullname="+fname+"%20"+lname)
+resp = requests.get("https://jsonplaceholder.typicode.com/posts")
 
 data = resp.json()
+pprint(data)
 
