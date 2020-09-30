adc9ab700f54385405c86282092baa2af5fbfc22
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2020-09-30 09:04:15 -0400

    update

--- NASA_API_Product/NASA_API_master.py
+++ NASA_API_Product/NASA_API_master.py
@@ -1,9 +1,45 @@
-import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageFilter
+from PIL import ImageTk
+from io import *
 import requests
-import json
 from pprint import pprint
 
-resp = requests.get("https://api.nasa.gov/planetary/apod?date=2020-09-09&api_key=DEMO_KEY")
+#Use a local file
+#im = Image.open( 'image.jpg' )
 
+#Use a url
+
+#resp = requests.get("https://api.nasa.gov/planetary/apod?date=2020-09-28&api_key=DEMO_KEY")
+resp = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+
+
+#Converts response to JSON
 data = resp.json()
+
 pprint(data)
+pic_url = data["url"]
+imageLoad = requests.get(pic_url)
+image = Image.open(BytesIO(imageLoad.content))
+#this opens a dedicated window for the image
+#im.show()
+
+picWidth = image.size[0] // 2
+picHeight = image.size[1] // 2
+picSize = (picWidth, picHeight)
+image.resize(picSize)
+windowSize = "%dx%d" % (picWidth,picHeight)
+
+
+#display the image on a GUI
+root = Tk()
+root.title("Display Image from URL")
+root.geometry(windowSize)
+renderImage = ImageTk.PhotoImage(image)
+display = Label(root, image=renderImage)
+display.image = renderImage
+display.pack(side=TOP)
+
+root.mainloop()
+
+
