03e98803e27c3131a924fa3733599dc0f3267b83
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2020-10-06 11:07:20 -0400

    update

--- NASA_API_Product/NASA_API_master.py
+++ NASA_API_Product/NASA_API_master.py
@@ -10,12 +10,23 @@ from pprint import pprint
 
 #Use a url
 
-#resp = requests.get("https://api.nasa.gov/planetary/apod?date=2020-09-28&api_key=DEMO_KEY")
-resp = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+resp = requests.get("https://api.nasa.gov/planetary/apod?date=2020-09-28&api_key=DEMO_KEY")
+#resp = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
 
 
 #Converts response to JSON
 data = resp.json()
+
+
+def process(*args):
+	print("PROCESSING")
+	year = yentry.get()
+	month = mentry.get()
+	day = dentry.get()
+
+	date = year + "-"+month+"-"+day
+
+	resp = requests.get("https://api.nasa.gov/planetary/apod?date=2020-09-28&api_key=DEMO_KEY")
 
 pprint(data)
 pic_url = data["url"]
@@ -30,15 +41,34 @@ picSize = (picWidth, picHeight)
 image.resize(picSize)
 windowSize = "%dx%d" % (picWidth,picHeight)
 
-
+ 
 #display the image on a GUI
 root = Tk()
 root.title("Display Image from URL")
 root.geometry(windowSize)
+
+lyear = Label(root, text = "YEAR")
+lyear.pack()
+yentry = Entry(root)
+yentry.pack()
+
+myear = Label(root, text = "Month")
+myear.pack()
+mentry = Entry(root)
+mentry.pack()
+
+dyear = Label(root, text = "Day")
+dyear.pack()
+dentry = Entry(root)
+dentry.pack()
+
+
 renderImage = ImageTk.PhotoImage(image)
 display = Label(root, image=renderImage)
 display.image = renderImage
 display.pack(side=TOP)
+
+root.bind("<Return>",process)
 
 root.mainloop()
 
