74027ad0c2f26f2bb77b651de395dfe90cb594f1
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2020-11-10 11:51:35 -0500

    update

--- Auth_Demo_Python_ClassB/auth_demo.py
+++ Auth_Demo_Python_ClassB/auth_demo.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
-uNames = ["user1@test.com"]
-pWords = ["pword1"]
-
+uNames = ["user1@test.com","user2@test.com","user3@test.com"]
+pWords = ["pword1","pword2","pword3"]
+active = ["",""] #Stores active user 
 
 def checkCred(*arg):
 
@@ -10,12 +10,36 @@ def checkCred(*arg):
 
 	#Write the code to
 	#Step 1: Take entry for user name
+	u = entunLF.get()
 	#Step 2: Take entry for password
+	p = entpwLF.get()
 	#Step 3: Loop through usernames and check if valid with password
 
+	for i in range(0,len(uNames),1):
+		if (uNames[i] == u):
+			if (pWords[i] == p):
+				#SWAP SCREENS
+				loginFrame.pack_forget()
+				homeFrame.pack()
+				#SET ACTIVE USER
+				active[0] = u
+				active[1] = p
+				entunLF.delete(0,tk.END)
+				entpwLF.delete(0,tk.END)
+				return
+
 	#If the username nad password is correct swap frames. 
-	loginFrame.pack_forget()
-	homeFrame.pack()
+	entunLF.delete(0,tk.END)
+	entpwLF.delete(0,tk.END)
+
+def logout(*args): 
+	#SWAP
+	loginFrame.pack()
+	homeFrame.pack_forget()
+
+	#REMOVE ACTIVE USER
+	active[0] = ""
+	active[1] = ""
 
 root = tk.Tk() #Creates your main window
 
@@ -42,8 +66,9 @@ submitLF.pack()
 homeFrame = tk.Frame(root)
 
 labHF = tk.Label(homeFrame, text = "Welcome to Soccer Magic")
-
+logoutHF = tk.Button(homeFrame, text = "logout",command = logout)
 labHF.pack()
+logoutHF.pack()
 
 
 
