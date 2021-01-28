ce53286e3937e3eae312c97aacccb2bc87ec5382
Paul Miskew <pmiskew@C1MWP4S2J1WL.local>
2021-01-28 14:09:26 -0500

    update

--- Login_DB_Project_Cycle3/auth.js
+++ Login_DB_Project_Cycle3/auth.js
@@ -1,24 +1,45 @@
 //Step 1: I going grab all the objects on the page I need for manipulation of page
 const login_form = document.querySelector("#login_form")
-
 const login_nav = document.getElementById("login_nav")
 const logout_nav = document.getElementById("logout_nav")
 const learn_more_nav = document.getElementById("learn_more_nav")
 const elements_nav = document.getElementById("elements_nav")
+
+
+var users =["user1","user2","user3"]
+var pwords = ["pword1","pword","pword3"]
+
+
+
 
 login_form.addEventListener("submit", (e) => {
 
 	console.log(e)
 	e.preventDefault() //Stops page from reloading
 
-
+	//Step 1:
+	//Take form information
+	user = login_form["user_name"].value
+	password = login_form["user_password"].value
+	console.log(user,password)
 	//Option 1: Verify against a predefined list - for learning
 	//WE ARE GOING TO AIM FOR THIS
 
+	//Take the input from the submitted form and check to if the username 
+	//and password are valid
 
-	//Option 2: authenticate using firebase or other 3rd party
-	//
+	//Step 2:
+	//Create flag valid = false
 
+	//Step 3: 
+	//Loop through users to check for user 
+		//if user[i] == user
+			//if pwords[i] == pword
+				//set valid to true
+
+	//Step 4:
+	//if valid is true then you are going to change the diplsay
+	//if valid is false then tell the user somehow
 
 
 	//changed display states of nav bar buttons
---
+++ Login_DB_Project_Cycle3/readme.txt
@@ -0,0 +1,17 @@
+Understand Landscape:
+
+When you first arrive to the page you land on 
+
+			                 LOGIN
+index.html (not logged in) -------------> index.html(logged in)
+											| |
+											| |
+											| |--> elements.html (lost user info) <-|
+											|        								|	
+											|        								|
+											| 		 								|
+											|        								|
+											|----> learnmore.html (lost user info)<-|
+
+-	The only place where we will now user information is index.html if you want take 
+	inputs from the user, you have to do it here or relogin later. 
